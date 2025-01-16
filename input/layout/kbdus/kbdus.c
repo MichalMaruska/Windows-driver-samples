faf73e41585fc5135685ce888878972b10a0c7d4
Michal Maruska <mmaruska@gmail.com>
2025-01-16 10:02:42 +0100

    drop the unsuccessful experiment Vkey -> Modifiers

--- input/layout/kbdus/kbdus.c
+++ input/layout/kbdus/kbdus.c
@@ -141,7 +141,7 @@ static ALLOC_SECTION_LDATA VSC_VK aE1VscToVk[] = {
 static ALLOC_SECTION_LDATA VK_TO_BIT aVkToBits[] = {
     { VK_SHIFT    ,   KBDSHIFT     },
     { VK_CONTROL  ,   KBDCTRL      },
-#if 0
+#if 1
     { VK_MENU     ,   KBDALT       },
 #else
     // I want these Virtual keys:
